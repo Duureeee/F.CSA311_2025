3a9e8ded3e64bcd0dd4ef2ae98fa9d3480437fa2
Duureeee <Dbileg04@gmail.com>
2026-02-04 16:54:35 +0800

    z

--- lab1/my-app/src/main/java/com/example/myapp/App.java
+++ lab1/my-app/src/main/java/com/example/myapp/App.java
@@ -6,5 +6,6 @@ package com.example.myapp;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
     }
 }
